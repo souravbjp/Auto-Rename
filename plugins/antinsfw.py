16711c4731fd66ddd6163c657a2da296ea2c8023
souravbjp <souravbosu.bjp@zohomail.com>
2026-03-26 19:49:11 +0600

    Update antinsfw.py

--- plugins/antinsfw.py
+++ plugins/antinsfw.py
@@ -1,46 +1,16 @@
 nsfw_keywords = {
     "general": [
-        "porn", "sex", "nude", "naked", "boobs", "tits", "pussy", "dick", "cock", "ass",
-        "fuck", "blowjob", "cum", "orgasm", "shemale", "erotic", "masturbate", "anal",
-        "hardcore", "bdsm", "fetish", "lingerie", "xxx", "milf", "gay", "lesbian",
-        "threesome", "squirting", "butt plug", "dildo", "vibrator", "escort", "handjob",
-        "striptease", "kinky", "pornstar", "sex tape", "spank", "swinger", "taboo", "cumshot",
-        "deepthroat", "domination", "submission", "handcuffs", "orgy", "roleplay", "sex toy",
-        "voyeur", "cosplay", "adult", "culture", "pornhwa",
-        "netorare", "netori", "netorase", "eromanga", "incest", "stepmom", "stepdad",
-        "stepsister", "stepbrother", "stepson", "stepdaughter", "ntr", "gangbang",
-        "facial", "golden shower", "pegging", "rimming", "rough sex", "dirty talk",
-        "sex chat", "nude pic", "lewd", "titty", "twerk", "breasts", "penis", "vagina",
-        "clitoris", "genitals", "sexual", "kamasutra", "incest", "pedo", "rape", "bondage",
-        "cum inside", "creampie", "sex slave", "sex doll", "sex machine", "latex", "oral sex",
-        "butt", "slut", "whore", "tramp", "skank", "cumdumpster", "cultured", "ecchi", "doujin",
-        "hentai", "smut", "lewd", "waifu", "futanari", "tentacle"
+        "jjg", "smut", "lewd", "waifu", "futanari", "tentacle"
     ],
     "hentai": [
-        "hentai", "doujinshi", "ecchi", "yaoi", "shota", "loli", "tentacle", "futanari",
-        "bishoujo", "bishounen", "mecha hentai", "hentai manga", "hentai anime", "smut",
-        "eroge", "visual novel", "h-manga", "h-anime", "adult manga", "18+ anime", "18+ manga",
-        "lewd anime", "lewd manga", "animated porn", "animated sex", "hentai game", "hentai art",
-        "hentai drawing", "hentai doujin", "yaoi hentai", "hentai comic",
-        "hentai picture", "hentai scene", "hentai story", "hentai video", "hentai movie",
-        "hentai episode", "hentai series"
+        "jhffr episode", "errff series"
     ],
     "abbreviations": [
-        "pr0n", "s3x", "n00d", "fck", "bj", "hj", "l33t", "p0rn", "h3ntai", "h-ntai", "pnwh",
-        "p0rnhwa", "l33tsp34k", "l3wd", "cultur3d", "s3xual"
+        "mkhgg", "p0rmkhgg", "l3wd", "cultur3d", "s3xual"
     ],
-    "offensive_slang": [
-        "slut", "whore", "tramp", "skank", "cumdumpster", "gangbang", "facial", "golden shower",
-        "pegging", "rimming", "rough sex", "dirty talk", "sex chat", "nude pic", "lewd", "titty",
-        "twerk", "breasts", "penis", "vagina", "clitoris", "genitals", "sexual", "kamasutra",
-        "incest", "pedo", "rape", "sex slave", "bondage", "creampie", "cum inside", "sex doll",
-        "sex machine", "latex", "oral sex", "cumshot", "deepthroat", "domination", "submission",
-        "handcuffs", "orgy", "roleplay", "sex toy", "voyeur", "cosplay", "adult", "culture",
-        "anal", "erotic", "masturbate", "hardcore", "bdsm", "fetish", "lingerie", "milf", "taboo"
-    ]
 }
 
-exception_keywords = ["nxivm", "classroom", "assassination", "geass"]
+exception_keyword "classroohfd", "clsyoom", "asgyation", "gss"]
 
 async def check_anti_nsfw(new_name, message):
     lower_name = new_name.lower()
